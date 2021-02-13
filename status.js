e38c8cf273b33f0366daa00609f229152400578f
Eduardow GAmer <68908379+GamerPro771@users.noreply.github.com>
2021-02-13 11:42:12 -0300

    Update status.js

--- status.js
+++ status.js
@@ -1,26 +1,24 @@
-//ESTE CODIGO REQUER QUE ESTA LINHA DE CODIGO SEJA ADICIONADA AO SEU INDEX.JS
-
-client.on('ready', () => {
-    //STATUS
-    const status = require('./status.js'); //Requer o arquivo status.js
-    status.run(client); //exporta as informações para o status
-});
-
-
 const Discord = require('discord.js') // Cria uma constant para a libraria do Discord
 
 exports.run = async (client) => { //Importa os itens do index.js
 
-  const atividades = [ //Conjunto de atividades que serão exibidas
+  let atividades = [ //Conjunto de atividades que serão exibidas
   `👀 ${client.guilds.cache.size} servidores diferentes!`, //Número de servidores em que o Bot está
   `👀 ${client.users.cache.size} usuários!` //Número de usuarios que estão disponiveis para o bot
 ];
 
 i = 0;
 setInterval(() => client.user.setActivity(`${atividades[i++ % atividades.length]}`,{
-  type: '// Tipo do status (PLAYING, STREAMING ou WATCHING)'
-',
-}), //Intervalo da troca em milisegundos);
+  type: 'WATCHING' // Tipo do status
+}, 30000); //Intervalo da troca em milisegungos (30000 = 30 segundos)
 
-console.log('Estou online meu povo!') //Loga que está online
+console.log('Estou online!') //Loga que está online
 }
+
+
+//AS LINHAS ABAIXO DEVEM SER ADICIONADAS AO SEU INDEX.JS E REMOVIDAS DAQUI!
+
+client.on('ready', () => {
+    const status = require('./status.js'); //Requer o arquivo status.js
+    status.run(client); //exporta as informações para o status
+});
