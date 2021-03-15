70aaefd0ef1ab03f4dc8a5f5ff90c96bd6bb7bab
Eduardow GAmer <68908379+GamerPro771@users.noreply.github.com>
2021-03-15 09:27:16 -0300

    Update Auto-reply.js

--- Auto-reply.js
+++ Auto-reply.js
@@ -5,17 +5,14 @@ exports.run = async (client, message) => {
   if(message.channel.type == "dm") return;
   if(!message.content.startsWith(`<@!${client.user.id}>`)) return;
     message.channel.send(`**😘 Olá ${message.author}, em que posso servilo(a)?**`)
-}
+};
 
-//AS LINHAS ABAIXO DEVEM SER ADICIONADAS AO INDEX.JS E REMOVIDAS DAQUI!
+/* As linhas abaixo devem ser adicionadas no index.js logo abaixo dessas linhas de codigo aqui
 
+const commands = require('./commandsrun');
+commands.run(client, message, args); 
 
-client.on('message', async (message) => {
-  const args = message.content
-	.trim()
-	.slice(config.prefix.length)
-	.split(/ +/g);
-  
-  const autoreply = require('./Auto-Reply.js'); //REQUER O ARQUIVO Auto-Reply.js
-  autoreply(client, message);
-});
+*/
+
+const autoreply = require('./Auto-Reply.js'); //REQUER O ARQUIVO Auto-Reply.js
+autoreply(client, message);
