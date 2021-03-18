73f76a7988b7733e6f857e2f8d01dc8a516c44df
Eduardow GAmer <68908379+GamerPro771@users.noreply.github.com>
2021-03-18 10:54:15 -0300

    Update command-timer.js

--- command-timer.js
+++ command-timer.js
@@ -23,4 +23,4 @@ if (lastcommand.has(message.author.id) && message.author.id != "ID DO DONO DO BO
 //As linhas de codigo abaixo devem ser adicionadas ao index.js e removidas daqui!
 
 const commandtimer = require('./command-timer.js') //Requer o arquivo command-timer.js
-commandtimer.run(client, message, args, clientCommands, clientPremiumCommands)
+commandtimer.run(client, message, args)
