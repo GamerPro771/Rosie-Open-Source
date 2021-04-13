7b9697eabc9d682a726c9ec1e7cd6a49b6e864ab
Eduardow Gamer <68908379+GamerPro771@users.noreply.github.com>
2021-04-13 20:09:45 -0300

    Update play.js
    Desta vez eu dei uma atualizada no .play, para que você e seus amigos possam ouvir musica de uma forma mais fácil!

--- play.js
+++ play.js
@@ -1,17 +1,28 @@
-const Discord = require('discord.js');
-const yout = require('ytdl-core')
-
-exports.run = async (client, message, args) => {
+exports.run: async (client, message, args) => {
   const content = args.join(' ')
   if(!args[0]) {
-   return message.channel.send(`** ${message.author} escreva o URL da música desejada!**`)
+    return message.channel.send(`Coloque o nome ou o link do vídeo desejado após o comando!`)
   }
   if(message.member.voice.channel) {
-    
     const connection = await message.member.voice.channel.join();
-    connection.play(yout(content, { filter: 'audioonly' }));
-    
+
+    var opts = {
+            maxResults: 1,
+            key: "Sua Key do youtube" //Existem varios tutoriais de como pegar uma key, este é um deles: https://www.youtube.com/watch?v=VqML5F8hcRQ
+    }
+
+    search(args, opts, function (err, results) {
+            if (err) return console.log(err);
+
+            results.forEach(result => {
+                const link = result.link
+            });
+
+            ServerList.forEach(url => {
+                    connection.play(yout(`${url}`, { filter: 'audioonly', highWaterMark: 50 }))
+            })
+    });
   } else {
-    message.channel.send(`**${message.author}, conecte-se em um canal de voz antes de executar o comando!**`)
- }  
+    message.channel.send(`Entre em um canal de vóz antes de executar o comando!`)
+  }
 }
