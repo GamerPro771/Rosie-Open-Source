001cdab052d52045cb88c3131b188458611d6e66
Eduardow Gamer <68908379+GamerPro771@users.noreply.github.com>
2021-04-29 10:52:42 -0300

    Create sensei.js
    Novo comando .sensei e pasta de comandos!

---
+++ Commands/sensei.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.run = async (client, message, args) => {
+                const channel = message.channel
+
+                const webhooks = await channel.fetchWebhooks();
+                const webhook = webhooks.find(hook => hook.name === "Nome do webhook" && hook.owner.id === "ID do seu bot");
+
+                const messages = [ //Suas frases  vão aqui!
+                    "",
+                    "",
+                    ""//....
+                ];
+
+                const random = messages[Math.floor(Math.random() * messages.length)]; //Escolhe uma frase aleatória!
+
+                if (!webhook) {
+
+                        channel.createWebhook("Sensei Arlindinho", {
+                                avatar: "", //Avatar do webhook
+                                reason: `` //Rasão do webhook ter sido criado
+                        })
+                
+                        .then(async hook => {
+                                hook.send(`${random}`)
+                        })
+
+                        .catch(err => message.channel.send(`Mensagem de erro`));
+
+                } else {
+                        webhook.send(`${random}`)
+                };
+        }
