dec0e68896d045b672632a1b183e9f8dc161a3e5
Eduardow Gamer <68908379+GamerPro771@users.noreply.github.com>
2021-05-24 10:16:00 -0300

    Update status.js

--- status.js
+++ status.js
@@ -1,24 +1,16 @@
 const Discord = require('discord.js') // Cria uma constant para a libraria do Discord
 
-exports.run = async (client) => { //Importa os itens do index.js
+exports.run = async (client) => {
 
   let atividades = [ //Conjunto de atividades que serão exibidas
-  `👀 ${client.guilds.cache.size} servidores diferentes!`, //Número de servidores em que o Bot está
-  `👀 ${client.users.cache.size} usuários!` //Número de usuarios que estão disponiveis para o bot
+  `👀 ${client.guilds.cache.size} servidores diferentes!`,
+  `👀 ${client.users.cache.size} usuários!`
 ];
 
 i = 0;
 setInterval(() => client.user.setActivity(`${atividades[i++ % atividades.length]}`,{
-  type: 'WATCHING' // Tipo do status
-}, 10 * 60 * 1000); // Intervalo para a troca de status exemplo: 25 * 60 * 1000 = 25 minutos
+  type: 'WATCHING'
+}, 10 * 60 * 1000);
 
- console.log('Estou online!') //Loga que está online
+ console.log('Estou online!')
 }
-
-
-//AS LINHAS ABAIXO DEVEM SER ADICIONADAS AO SEU INDEX.JS E REMOVIDAS DAQUI!
-
-client.on('ready', () => {
-    const status = require('./status.js'); //Requer o arquivo status.js
-    status.run(client); //exporta as informações para o status
-});
