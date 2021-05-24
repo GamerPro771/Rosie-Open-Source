c2f8812e703db6be58515da58cc00e5f64a07681
Eduardow Gamer <68908379+GamerPro771@users.noreply.github.com>
2021-05-24 10:20:03 -0300

    Update Auto-reply.js

--- Auto-reply.js
+++ Auto-reply.js
@@ -1,18 +1,9 @@
 const Discord = require('discord.js')
 
 exports.run = async (client, message) => {
-  if(message.author.bot) return;
-  if(message.channel.type == "dm") return;
-  if(!message.content.startsWith(`<@!${client.user.id}>`)) return;
-    message.channel.send(`**😘 Olá ${message.author}, em que posso servilo(a)?**`)
+  if(message.author.bot || message.channel.type == "dm") return;
+  
+  if(!message.content.startsWith(`<@!${client.user.id}>`) && !message.content.startsWith(`<@${client.user.id}>`)) return;
+  
+    message.channel.send(`**😘 Olá ${message.author}!**`)
 };
-
-/* As linhas abaixo devem ser adicionadas no index.js logo abaixo dessas linhas de codigo aqui
-
-const commands = require('./commandsrun');
-commands.run(client, message, args); 
-
-*/
-
-const autoreply = require('./Auto-Reply.js'); //REQUER O ARQUIVO Auto-Reply.js
-autoreply(client, message);
